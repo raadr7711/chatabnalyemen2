09ca8917e8913a4b772cd4ba04b6a729d037711b
raadr7711 <38633188+raadr7711@users.noreply.github.com>
2019-03-08 04:00:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 //socket.io instantiation
 
 
-var io = require("socket.io")(443)
+var io = require("socket.io")
 
 //listen on every connection
 io.on('connection', (socket) => {
