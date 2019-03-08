7cf4704bd5732a224c23e493655f5fab26921079
raadr7711 <38633188+raadr7711@users.noreply.github.com>
2019-03-08 03:00:13 +0300

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -2,7 +2,7 @@
 $(function(){
    	//make connection
 	
-	var socket = io.connect('abnalyemen3.herokuapp.com:80')
+	var socket = io.connect('abnalyemen3.herokuapp.com:3000')
 console.log(`Listening  raad on ${ PORT }`)
 	//buttons and inputs
 	var message = $("#message")
